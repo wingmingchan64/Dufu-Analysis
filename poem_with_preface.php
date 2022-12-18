5be2212bcb4415d61e0e7cdb68dd176752df1b33
Wing Ming Chan <wingmingchan64@hotmail.com>
2022-12-18 10:15:46 -0800

    Updated

---
+++ poem_with_preface.php
@@ -0,0 +1,19 @@
+<?php
+global $poem_with_preface;
+$poem_with_preface = array(
+'h:\github\Dufu\09 卷九 2501-2746\2516 江頭五詠.txt' =>
+	array( 3, 10, 17, 24, 31 ),
+'h:\github\DuFu\13 卷十三 3591-3883\3686 種萵苣.txt' =>
+	array( 3, 4 ),
+'h:\github\Dufu\14 卷十四 3885-4144\3955 八哀詩.txt' =>
+	array( 3, 5, 40, 63, 100, 129, 175, 210, 245  ),
+'h:\github\Dufu\16 卷十六 4533-4919\4546 園官送菜.txt' =>
+	array( 3, 4 ),
+'h:\github\DuFu\16 卷十六 4533-4919\4556 課伐木.txt' =>
+	array( 3 ),
+'h:\github\DuFu\18 卷十八 5261-5496\5308 觀公孫大娘弟子舞劍器行.txt' =>
+	array( 3, 4, 5, 6, 7, 8 ),
+'h:\github\DuFu\20 卷二十 5809-6108\5941 追酬故高蜀州人日見寄.txt' =>
+	array( 3, 4, 5, 6 )
+);
+?>
