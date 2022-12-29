45061b855bb48178d2b9546994610c9da14e2055
Wing Ming Chan <wingmingchan64@hotmail.com>
2022-12-29 09:23:33 -0800

    Updated

--- analysis_programs/生成書本注釋.php
+++ analysis_programs/生成書本注釋.php
@@ -7,14 +7,12 @@ require_once( 'h:\github\Dufu-Analysis\書目簡稱.php' );
 $簡稱   = '=浦';
 $文件夾 = $書目簡稱[ $簡稱 ];
 $out_path = "h:\\github\\Dufu-Analysis\\${文件夾}\\";
-
-$code = "<?php\n\$注釋=array(\n";
-
+$code = "<?php\n\$${書目簡稱[ $簡稱 ]}注釋=array(\n";
 
 foreach( $頁碼 as $頁 )
 {
 	$頁路徑 = $out_path . "${頁}.php";
-	//echo $頁路徑, "\n";
+
 	if( file_exists( $頁路徑 ) )
 	{
 		require_once( $頁路徑 );
@@ -26,9 +24,8 @@ foreach( $頁碼 as $頁 )
 			$code = $code . "\"${坐標}\"=>\"${注釋}\",\n";
 		}
 	}
-	//$code = substr( $code, 0, -1 );
 }
 
 $code = $code . ");\n?>";
-file_put_contents( $out_path . "注釋.php", $code );
+file_put_contents( $out_path . "${書目簡稱[ $簡稱 ]}注釋.php", $code );
 ?>
