18e4a10998c66535505ab30f92e97a1528e22272
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-01-01 08:37:56 -0800

    Updated

---
+++ 陳永明《杜甫全集粵音注音》/0326.php
@@ -0,0 +1,66 @@
+<?php
+/*
+生成：本文檔用 PHP 生成。
+說明：陳永明《杜甫全集粵音注音》中關於《送韋書記赴安西》的資料。
+*/
+$内容=array(
+"書名"=>"陳永明《杜甫全集粵音注音》",
+"注音"=>
+array(
+
+"夫子欻通貴。雲泥相望懸。"=>array("fu1 zi2 fat1 tung1 gwai3, wan4 nai4 soeng1 mong6 jyun4","平仄仄平仄，平平平仄平（「仄平仄」拗，「平仄平」救）"),
+"白頭無籍在。朱紱有哀憐。"=>array("baak6 tau4 mou4 zik6 zoi6, zyu1 fat1 jau5 oi1 lin4","仄仄平仄仄，平仄仄平平"),
+"書記赴三捷。公車留二年。"=>array("syu1 gei3 fu6 saam1 zit6, gung1 ce1 lau4 ji6 nin4","平仄仄平仄，平平平仄平（「仄平仄」拗，「平仄平」救）"),
+"欲浮江海去。此別意蒼然。"=>array("juk6 fau4 gong1 hoi2 heoi3, ci2 bit6 ji3 cong1 jin4","仄平平仄仄，仄仄仄平平"),),
+"字音"=>array(
+"夫"=>array("fu1"),
+"子"=>array("zi2"),
+"欻"=>array("fat1"),
+"通"=>array("tung1"),
+"貴"=>array("gwai3"),
+"雲"=>array("wan4"),
+"泥"=>array("nai4"),
+"相"=>array("soeng1"),
+"望"=>array("mong6"),
+"懸"=>array("jyun4"),
+"白"=>array("baak6"),
+"頭"=>array("tau4"),
+"無"=>array("mou4"),
+"籍"=>array("zik6"),
+"在"=>array("zoi6"),
+"朱"=>array("zyu1"),
+"紱"=>array("fat1"),
+"有"=>array("jau5"),
+"哀"=>array("oi1"),
+"憐"=>array("lin4"),
+"書"=>array("syu1"),
+"記"=>array("gei3"),
+"赴"=>array("fu6"),
+"三"=>array("saam1"),
+"捷"=>array("zit6"),
+"公"=>array("gung1"),
+"車"=>array("ce1"),
+"留"=>array("lau4"),
+"二"=>array("ji6"),
+"年"=>array("nin4"),
+"欲"=>array("juk6"),
+"浮"=>array("fau4"),
+"江"=>array("gong1"),
+"海"=>array("hoi2"),
+"去"=>array("heoi3"),
+"此"=>array("ci2"),
+"別"=>array("bit6"),
+"意"=>array("ji3"),
+"蒼"=>array("cong1"),
+"然"=>array("jin4")
+),
+"韻部"=>
+array(
+"〚0326:3.2.5〛"=>"懸：先韻",
+"〚0326:4.2.5〛"=>"憐：先韻",
+"〚0326:5.2.5〛"=>"年：先韻",
+"〚0326:6.2.5〛"=>"然：先韻")
+,
+
+);
+?>
