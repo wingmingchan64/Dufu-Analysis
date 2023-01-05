41a7681e4c651b0b6858a15ead93e225ea5c3f3d
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-01-04 18:47:03 -0800

    Updated

---
+++ 陳永明《杜甫全集粵音注音》/0576.php
@@ -0,0 +1,65 @@
+<?php
+/*
+生成：本文檔用 PHP 生成。
+說明：陳永明《杜甫全集粵音注音》中關於《戲簡鄭廣文虔兼呈蘇司業源明》的資料。
+*/
+$内容=array(
+"書名"=>"陳永明《杜甫全集粵音注音》",
+"注音"=>
+array(
+
+"廣文到官舍。繫馬堂階下。"=>array("gwong2 man4 dou3 gun1 se3, hai6 maa5 tong4 gaai1 haa6"),
+"醉則騎馬歸。頗遭官長罵。"=>array("zeoi3 zak1 kei4 maa5 gwai1, po2 zou1 gun1 zoeng2 maa6"),
+"垂名四十年。坐客寒無氈。"=>array("seoi4 ming4 sei3 sap6 nin4, zo6 haak3 hon4 mou4 zin1"),
+"賴有蘇司業。時時與酒錢。"=>array("laai6 jau5 sou1 si1 jip6, si4 si4 jyu5 zau2 cin4"),),
+"字音"=>array(
+"廣"=>array("gwong2"),
+"文"=>array("man4"),
+"到"=>array("dou3"),
+"官"=>array("gun1"),
+"舍"=>array("se3"),
+"繫"=>array("hai6"),
+"馬"=>array("maa5"),
+"堂"=>array("tong4"),
+"階"=>array("gaai1"),
+"下"=>array("haa6"),
+"醉"=>array("zeoi3"),
+"則"=>array("zak1"),
+"騎"=>array("kei4"),
+"歸"=>array("gwai1"),
+"頗"=>array("po2"),
+"遭"=>array("zou1"),
+"長"=>array("zoeng2"),
+"罵"=>array("maa6"),
+"垂"=>array("seoi4"),
+"名"=>array("ming4"),
+"四"=>array("sei3"),
+"十"=>array("sap6"),
+"年"=>array("nin4"),
+"坐"=>array("zo6"),
+"客"=>array("haak3"),
+"寒"=>array("hon4"),
+"無"=>array("mou4"),
+"氈"=>array("zin1"),
+"賴"=>array("laai6"),
+"有"=>array("jau5"),
+"蘇"=>array("sou1"),
+"司"=>array("si1"),
+"業"=>array("jip6"),
+"時"=>array("si4"),
+"與"=>array("jyu5"),
+"酒"=>array("zau2"),
+"錢"=>array("cin4")
+),
+"韻部"=>
+array(
+"〚0576:3.1.5〛"=>"舍：禡韻",
+"〚0576:3.2.5〛"=>"下：禡韻",
+"〚0576:4.2.5〛"=>"罵：禡韻",
+"〚0576:5.1.5〛"=>"年：先韻",
+"〚0576:5.2.5〛"=>"氈：先韻",
+"〚0576:6.2.5〛"=>"錢：先韻")
+,
+
+);
+?>
