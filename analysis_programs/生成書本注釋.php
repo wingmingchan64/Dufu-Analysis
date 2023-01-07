2f97359c7e3a660ba01d096ff451a4c207b116d5
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-01-07 09:41:34 -0800

    Updated

--- analysis_programs/生成書本注釋.php
+++ analysis_programs/生成書本注釋.php
@@ -5,7 +5,7 @@ require_once( 'h:\github\Dufu-Analysis\頁碼.php' );
 require_once( 'h:\github\Dufu-Analysis\書目簡稱.php' );
 
 $簡稱   = '=譯';
-$種類   = '譯文';
+$種類   = '注釋';
 $文件夾 = $書目簡稱[ $簡稱 ];
 $out_path = "h:\\github\\Dufu-Analysis\\${文件夾}\\";
 $code = "<?php\n\$${書目簡稱[ $簡稱 ]}${種類}=array(\n";
@@ -18,15 +18,19 @@ foreach( $頁碼 as $頁 )
 	{
 		require_once( $頁路徑 );
 		
-		foreach( $内容[ $種類 ] as $坐標 => $内容 )
+		if( array_key_exists( $種類, $内容 ) )
 		{
-			$坐標 = 生成完整坐標( $坐標, $頁 );
-			$内容 = trim( $内容 );
-			$code = $code . "\"${坐標}\"=>\"${内容}\",\n";
+			foreach( $内容[ $種類 ] as $坐標 => $内容 )
+			{
+				$坐標 = 生成完整坐標( $坐標, $頁 );
+				$内容 = trim( $内容 );
+				$code = $code . 
+					"\"${坐標}\"=>\"${内容}\",\n";
+			}
 		}
 	}
 }
 
 $code = $code . ");\n?>";
-file_put_contents( $out_path . "${書目簡稱[ $簡稱 ]}注釋.php", $code );
+file_put_contents( $out_path . "${書目簡稱[ $簡稱 ]}${種類}.php", $code );
 ?>
