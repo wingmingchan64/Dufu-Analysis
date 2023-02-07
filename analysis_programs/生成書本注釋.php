ae186ce2a27a75af1eb14ad6cd59817edc93a360
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-02-07 14:09:41 -0800

    Updated

--- analysis_programs/生成書本注釋.php
+++ analysis_programs/生成書本注釋.php
@@ -1,6 +1,6 @@
 <?php
 /*
-php h:\github\Dufu-Analysis\analysis_programs\生成字典注釋.php
+php h:\github\Dufu-Analysis\analysis_programs\生成字典注釋.php 今 注釋
 
 */
 require_once( '常數.php' );
@@ -8,12 +8,20 @@ require_once( '函式.php' );
 require_once( 杜甫資料庫 . '頁碼.php' );
 require_once( 杜甫資料庫 . '書目簡稱.php' );
 
+if( sizeof( $argv ) < 3 )
+{
+	echo "必須提供簡稱、種類。", "\n";
+	exit;
+}
+
+$簡稱 = '=' . $argv[ 1 ];
+$種類 = $argv[ 2 ];
+/*
 $簡稱   = '=譯';
 $簡稱   = '=今';
-
 $種類   = '大意';
 $種類   = '注釋';
-
+*/
 $文件夾 = $書目簡稱[ $簡稱 ];
 $out_path = 杜甫資料庫 . "${文件夾}\\";
 $code = "<?php\n\$${書目簡稱[ $簡稱 ]}${種類}=array(\n";
