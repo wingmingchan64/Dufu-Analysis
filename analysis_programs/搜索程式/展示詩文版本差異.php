67b2f61af73bcba7aa698e3b31c239a9ee16ec94
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-05-22 18:36:36 -0600

    Updated

--- analysis_programs/搜索程式/展示詩文版本差異.php
+++ analysis_programs/搜索程式/展示詩文版本差異.php
@@ -44,9 +44,9 @@ if( array_key_exists( "=${簡稱1}", $書目簡稱 ) &&
 	foreach( $result as $pos => $chars )
 	{
 		$詩文1 = str_replace( $chars[ 0 ], 
-			'🔻' . $chars[ 0 ], $詩文1 );
+			'🔴' . $chars[ 0 ], $詩文1 );
 		$詩文2 = str_replace( $chars[ 1 ],  
-			'🔻' . $chars[ 1 ] , $詩文2 );
+			'🔴' . $chars[ 1 ] , $詩文2 );
 	}
 	echo $書目簡稱[ "=${簡稱1}" ], "\n";
 	echo $詩文1, "\n\n";
