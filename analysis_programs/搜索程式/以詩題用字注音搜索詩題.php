e78d74145cf5bf5a47d0834193b7ccaeabb0f713
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-07-02 09:09:35 -0500

    Updated

--- analysis_programs/搜索程式/以詩題用字注音搜索詩題.php
+++ analysis_programs/搜索程式/以詩題用字注音搜索詩題.php
@@ -3,14 +3,15 @@
 php h:\github\Dufu-Analysis\analysis_programs\搜索程式\以詩題用字注音搜索詩題.php "fung6 sin1"
 => Array
 (
-    [jau4 lung4 mun4 fung6 sin1 zi6] => 遊龍門奉先寺
-    [fung6 sin1 lau4 siu3 fu2 san1 waak6 saan1 seoi2 zoeng3 go1] => 奉先劉少府新畫山水障歌
-    [zi6 ging1 fu6 fung6 sin1 jyun6 wing6 waai4 ng5 baak3 zi6] => 自京赴奉先縣詠懷五百字
+    [jau4 lung4 mun4 fung6 sin1 zi6] => 0042 遊龍門奉先寺
+    [fung6 sin1 lau4 siu3 fu2 san1 waak6 saan1 seoi2 zoeng3 go1] => 0527 奉先劉少府新畫山水障歌
+    [zi6 ging1 fu6 fung6 sin1 jyun6 wing6 waai4 ng5 baak3 zi6] => 0668 自京赴奉先縣詠懷五百字
 )
 */
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
 require_once( 詩題_注音 );
+require_once( 詩題_頁碼 );
 
 checkARGV( $argv, 2, 提供題音 );
 $音 = trim( $argv[ 1 ] );
@@ -21,7 +22,7 @@ foreach( $注音_詩題 as $注音 => $詩題 )
 {
 	if( containsPronunciation( $注音, $音 ) )
 	{
-		$result[ $注音 ] = $詩題;
+		$result[ $注音 ] = $詩題_頁碼[ $詩題 ] . ' ' . $詩題;
 	}
 }
 if( sizeof( $result ) == 0 )
---
+++ analysis_programs/搜索程式/以頁碼提取加括號詩文.php
@@ -0,0 +1,30 @@
+<?php
+/*
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\以頁碼提取加括號詩文.php 0668
+=> 
+*/
+require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
+require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
+require_once( 頁碼_詩題 );
+
+checkARGV( $argv, 2, 提供頁碼 );
+$頁碼 = trim( $argv[ 1 ] );
+$路徑 = 詩集文件夾 . $頁碼 . 程式後綴;
+$output = '〖1〗';
+
+if( file_exists( $路徑 ) )
+{
+	require_once( $路徑 );
+	$output .= $内容[ 詩題 ] . NL;
+	foreach( $内容[ 詩句 ] as $句 )
+	{
+		$output .= "〖${句}〗" . NL;
+	}
+	
+	echo $output;
+}
+else
+{
+	echo 無結果, NL;
+}
+?>
