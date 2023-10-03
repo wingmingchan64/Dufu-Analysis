616694de6be41a9881c7c8bea1b55182a02d19b4
Wing Ming Chan <wingmingchan64@hotmail.com>
2023-10-03 13:18:26 -0500

    Updated

--- analysis_programs/搜索程式/頁碼→注音、譯文.php
+++ analysis_programs/搜索程式/頁碼→注音、譯文.php
@@ -1,7 +1,24 @@
 <?php
 /*
 php h:\github\Dufu-Analysis\analysis_programs\搜索程式\頁碼→注音、譯文.php 0003
-=> 
+=>
+0003望嶽
+
+岱宗夫如何。齊魯青未了。
+doi6 zung1 fu4 jyu4 ho4, cai4 lou5 cing1 mei6 liu5
+泰山的高大究竟如何？走遍齊魯大地都能看到它的青色。
+
+造化鍾神秀。陰陽割昏曉。
+zou6 faa3 zung1 san4 sau3, jam1 joeng4 got3 fan1 hiu2
+天地把神奇和秀美聚集在它身上，那高大的山峯陰面是黃 昏，陽面卻是清曉。
+
+盪胸生曾雲。決眥入歸鳥。
+dong6 hung1 sang1 cang4 wan4, kyut3 zi6 jap6 gwai1 niu5
+山間雲氣生騰，層層疊疊，令我心胸激蕩；我極盡目力， 追羨那飛入山間的歸鳥。
+
+會當凌絕頂。一覽眾山小。
+wui6 dong1 ling4 zyut6 ding2, jat1 laam5 zung3 saan1 siu2
+啊，定會有一天登上絕頂，俯看腳下羣山的渺小！
 */
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
