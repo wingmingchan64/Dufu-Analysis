8bbb144e90c3f26f103caa24d19bb27282f68339
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-02-04 08:39:11 -0600

    Updated

--- analysis_programs/搜索程式/單字→注音.php
+++ analysis_programs/搜索程式/單字→注音.php
@@ -1,6 +1,6 @@
 <?php
 /*
-php h:\github\Dufu-Analysis\analysis_programs\搜索程式\字→粵音.php 居
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\單字→注音.php 居
 */
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
--- analysis_programs/搜索程式/詩句→注音.php
+++ analysis_programs/搜索程式/詩句→注音.php
@@ -1,6 +1,6 @@
 <?php
 /*
-php h:\github\Dufu-Analysis\analysis_programs\搜索程式\以詩句提取注音.php 把釣待秋風
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\詩句→注音.php 把釣待秋風
 => baa2 diu3 doi6 cau1 fung1
 */
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
