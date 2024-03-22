d20a3125be0dcd88bd6944514cad88c6bb6ac02f
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-03-22 15:19:12 -0500

    Updated

--- analysis_programs/搜索程式/詩文雙字→頁碼.php
+++ analysis_programs/搜索程式/詩文雙字→頁碼.php
@@ -1,13 +1,16 @@
 <?php
 /*
-php h:\github\Dufu-Analysis\analysis_programs\搜索程式\雙字→頁碼.php 愁 酒
+// 兩字同時出現在同一首或同一組詩內。
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\詩文雙字→頁碼.php 愁 酒
 =>
 */
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
 require_once( 頁碼 );
+require_once( 頁碼_詩題 );
 
-checkARGV( $argv, 3, 提供頁碼 );
+
+checkARGV( $argv, 3, 提供雙字 );
 $首字 = trim( $argv[ 1 ] );
 $次字 = trim( $argv[ 2 ] );
 $result = array();
@@ -19,7 +22,7 @@ foreach( $頁碼 as $頁 )
 	if( mb_strpos( $内容[ 詩文 ], $首字 ) !== false &&
 		mb_strpos( $内容[ 詩文 ], $次字 ) !== false )
 	{
-		array_push( $result, $頁 );
+		array_push( $result, $頁 . ' ' . $頁碼_詩題 [ $頁 ] );
 	}
 }
 print_r( $result );
