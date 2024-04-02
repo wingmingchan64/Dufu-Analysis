214f490831f9f56589697b5883c9fea867edf4c4
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-04-02 08:56:15 -0500

    Updated

---
+++ 朱鶴齡《杜工部詩集輯注》/朱鶴齡《杜工部詩集輯注》評論.php
@@ -0,0 +1,11 @@
+<?php
+/*
+生成：本文檔用 PHP 生成。
+生成程式：
+php H:\github\Dufu-Analysis\analysis_programs\生成版本評論.php 朱
+說明：本文檔儲存朱鶴齡《杜工部詩集輯注》評論。
+應用程式： 
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\頁碼、簡稱→評論.php 0003 朱
+*/
+$朱評論=array);
+?>
---
+++ 錢謙益《錢注杜詩》/錢謙益《錢注杜詩》評論.php
@@ -0,0 +1,11 @@
+<?php
+/*
+生成：本文檔用 PHP 生成。
+生成程式：
+php H:\github\Dufu-Analysis\analysis_programs\生成版本評論.php 錢
+說明：本文檔儲存錢謙益《錢注杜詩》評論。
+應用程式： 
+php h:\github\Dufu-Analysis\analysis_programs\搜索程式\頁碼、簡稱→評論.php 0003 錢
+*/
+$錢評論=array);
+?>
