5bb60cecd30f4d9931fc12aa3fe09b385b350c19
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-09-08 09:40:15 -0500

    Updated

--- analysis_programs/搜索程式/search.php
+++ analysis_programs/搜索程式/search.php
@@ -5,11 +5,14 @@ php h:\github\Dufu-Analysis\analysis_programs\搜索程式\search.php
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
 require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
 require_once( "四角字典.php" );
+require_once( "速成粵.php" );
+
 require_once( "H:\github\unicode\粵音_常用字.php" );
 
 $out_file = 'h:\github\Dufu-Analysis\analysis_programs\搜索程式\buffer.txt';
 $input    = "";
 $buffer   = "";
+$dict = array_merge( $dict, $速成粵 );
 
 while( true )
 {
