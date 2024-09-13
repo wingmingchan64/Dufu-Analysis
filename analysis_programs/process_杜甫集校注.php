8205040cad2c367482090943fdae2ee0c44c2973
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-09-13 09:59:18 -0500

    Updated

--- analysis_programs/process_杜甫集校注.php
+++ analysis_programs/process_杜甫集校注.php
@@ -1,11 +1,12 @@
 <?php
 /*
-php h:\github\Dufu-Analysis\analysis_programs\process_杜臆.php
+php h:\github\Dufu-Analysis\analysis_programs\process_杜甫集校注.php
 */
 require_once( "常數.php" );
 require_once( "函式.php" );
+require_once( 頁碼 );
 
-$text    = getFile( 'H:\杜甫資料庫\王嗣奭《杜臆》\奭目錄.txt' );
+$text    = getFile( 'H:\杜甫資料庫\謝思煒《杜甫集校注》\謝目錄.txt' );
 $lines   = explode( "\n", $text );
 $store   = array();
 $content = '';
@@ -18,8 +19,34 @@ foreach( $lines as $l )
 	}
 	else // only lines with //
 	{
+		// remove //
 		$l = str_replace( '// ', '', $l );
+		// space-separated page info
 		$l_array = explode( ' ', $l );
+		$題 = $l_array[ 0 ];
+		$頁 = $l_array[ 1 ];
+		//echo $題, ' ', $頁, ' ';
+		$版本頁 = $l_array[ 2 ];
+		//echo $版本頁, NL;
+		if( !in_array( $頁, $頁碼 ) )
+		{
+			if( $頁 != '' && mb_strpos( $頁, '/' ) !== false )
+			{
+				$parts = explode( '/', $頁 );
+				
+				foreach( $parts as $p )
+				{
+					if( !in_array( $p, $頁碼 ) )
+					{
+						echo $p, NL;
+					}
+				}
+			}
+			else
+				echo $頁, NL;
+		}
+
+/*
 		
 		if( $l_array[ 1 ] == '6497' )
 		{
@@ -78,6 +105,7 @@ foreach( $lines as $l )
 				$content = $content . 提取陣列値( $奭内容[ 評論 ] ) . NL . NL;
 			}
 		}
+*/
 	}
 }
 
