e6507ecfee579ed0cd1a944068c1afced95bb52c
Wing Ming Chan <wingmingchan64@hotmail.com>
2024-09-18 08:08:48 -0500

    Updated

---
+++ analysis_programs/create_版本_template.php
@@ -0,0 +1,76 @@
+<?php
+/*
+php h:\github\Dufu-Analysis\analysis_programs\create_版本_template.php
+*/
+require_once( "常數.php" );
+require_once( "函式.php" );
+require_once( 書目簡稱 );
+
+$簡字 = "謝";
+$簡稱 = "=${簡字}";
+$書名 = $書目簡稱[ $簡稱 ];
+$目錄文檔 = 杜甫資料庫 . $書名 . "\\" . "${簡字}目錄.txt";
+
+$text    = getFile( $目錄文檔 );
+$lines   = explode( "\n", $text );
+$store   = array();
+$content = '本文檔以 h:\github\Dufu-Analysis\analysis_programs\create_版本_template.php 生成。' . NL . NL;
+
+foreach( $lines as $l )
+{
+	if( $l == '' || mb_strpos( $l, '//' ) === false )
+	{
+		continue;
+	}
+	else // only lines with //
+	{
+		// remove //
+		$l = str_replace( '// ', '', $l );
+		// space-separated page info
+		$l_array = explode( ' ', $l );
+		// 詩題
+		$題 = $l_array[ 0 ];
+		// 默認頁碼
+		$頁 = trim( $l_array[ 1 ] );
+		// 版本頁碼
+		$版本頁 = trim( $l_array[ 2 ] );
+		
+		if( $頁 == '' )
+			continue;
+
+		$〖詩句〗 = '';
+		$路徑 = 詩集文件夾 . $頁 . 程式後綴;
+
+		if( file_exists( $路徑 ) )
+		{
+			require_once( $路徑 );
+			
+			$content .= NL . $頁 . ' ' . $内容[ 詩題 ] . NL .
+				"${簡稱}===================================================" . 
+				NL;
+			$content .= $書名 . $版本頁 . NL . NL;
+			$content .= 【異文、夾注】 . NL;
+			
+			if( $内容[ 詩題 ] != $題 )
+			{
+				$content .= '〖1〗' . $題 . NL;
+			}
+
+			foreach( $内容[ 詩句 ] as $句 )
+			{
+				$〖詩句〗 .= "〖${句}〗" . NL;
+			}
+			$content .= $〖詩句〗 . NL;
+			$content .= 【注釋】 . NL . '〖1〗' . NL . $〖詩句〗 . NL . 
+				【評論】 . NL . '〚〛' . NL . NL . 
+				【按語】 . NL;
+		}
+	}
+}
+
+$outfile = 杜甫資料庫 . $書名 . "\\" . "${簡字}template.txt";
+file_put_contents( $outfile, $content );
+$outfile = 杜甫分析文件夾 . $書名 . "\\" . "${簡字}template.txt";
+file_put_contents( $outfile, $content );
+?>
+
