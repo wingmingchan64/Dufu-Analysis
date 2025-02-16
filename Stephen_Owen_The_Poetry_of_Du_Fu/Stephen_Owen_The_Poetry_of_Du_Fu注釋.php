11cf980a18e3bda05e97f5534894efaa5f3a51d5
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-02-15 19:06:34 -0600

    Updated

--- Stephen_Owen_The_Poetry_of_Du_Fu/Stephen_Owen_The_Poetry_of_Du_Fu注釋.php
+++ Stephen_Owen_The_Poetry_of_Du_Fu/Stephen_Owen_The_Poetry_of_Du_Fu注釋.php
@@ -6,6 +6,15 @@
 說明：把注釋集中在一個文檔裏。
 */
 $Stephen_Owen_The_Poetry_of_Du_Fu注釋=array(
+"〚0003:1〛"=>"Before setting out in earnest to seek advancement, Du Fu traveled in the east of China, travels he wrote about often in his later years. No poems survive from his travels in the lower Yangzi, but a number of pieces remain from his tour of the region centered in modern Shandong. Mount Tai was the “Eastern Marchmount,” one of the five symbolic mountains that defined Chinese territory  —  a Marchmount for each of the four directions and one for the center.",
+"〚0003:3.1.1-5〛"=>"岱宗夫如何：The Peak in question is Mount Tai, here referred to by it honorific name Daizong.",
+"〚0003:3.2.1-5〛"=>"齊魯青未了：The north side of  Mount Tai was the ancient state of Qi; its south side was the state of Lu.",
+"〚0003:6.2.1-5〛"=>"一覽眾山小：Echoing Mencius (VIIA), telling how Confucius, when he climbed Mount Tai, thought all the world small.",
+"〚0008:1〛"=>"Du Fu’s father held a minor position at Yanzhou, and this was his base for Du Fu’s travels in eastern China.",
+"〚0008:3.1.1-5〛"=>"東郡趨庭日：“Rushing through the yard” refers to receiving instruction from one’s father, based on a passage in the Analects in which Confucius’s son Li was rushing through the yard, and Confucius asked him if he had studied the Poems. Du Fu’s father was an assistant in Yanzhou.",
+"〚0008:4.2.1-5〛"=>"平野入青徐：The two prefectures Qingzhou and Xuzhou.",
+"〚0008:5.1.1-5〛"=>"孤嶂秦碑在：The inscription on Mount Yi by the First Emperor, when he made the tour of his newly conquered empire.",
+"〚0008:5.2.1-5〛"=>"荒城魯殿餘：The famous Lingguang Palace constructed by Prince Gong of Lu in the Han.",
 "〚0042:1〛"=>"This is one of Du Fu’s earliest poems, on visiting the great temple region at Longmen, near Luoyang. Du Fu’s family had an estate in the Luoyang area.",
 "〚0042:5.1.1-5〛"=>"天闕象緯逼：Perhaps the two peaks at Longmen, referred to as its “Heaven [gate-]towers.”",
 );
--- Stephen_Owen_The_Poetry_of_Du_Fu/Stephen_Owen_The_Poetry_of_Du_Fu譯文.php
+++ Stephen_Owen_The_Poetry_of_Du_Fu/Stephen_Owen_The_Poetry_of_Du_Fu譯文.php
@@ -6,6 +6,14 @@
 說明：把譯文集中在一個文檔裏。
 */
 $Stephen_Owen_The_Poetry_of_Du_Fu譯文=array(
+"〚0003:3〛"=>"And what then is Daizong like? — over Qi and Lu, green unending.",
+"〚0003:4〛"=>"Creation compacted spirit splendors here, Dark and Light, riving dusk and dawn.",
+"〚0003:5〛"=>"Exhilirating the breast, it produces layers of cloud; splitting eye-pupils, it has homing birds entering.",
+"〚0003:6〛"=>"Someday may I climb up to its highest summit, with one sweeping view see how small all other mountains are.",
+"〚0008:3〛"=>"An eastern province, days of “rushing through the yard,” from its south tower I first let my eyes roam free.",
+"〚0008:4〛"=>"Drifting clouds stretch to Mount Tai and the sea, the level moors go off into Qing and Xu.",
+"〚0008:5〛"=>"The Qin stele is still there on the lonely cliff, an overgrown wall, ruins of the Lu palace.",
+"〚0008:6〛"=>"Ever many thoughts of ancient times, looking out, alone, I hesitate uncertain.",
 "〚0042:3〛"=>"I had already visited the temple and went on to stay over in the temple precincts.",
 "〚0042:4〛"=>"Shadowy ravines produced piping from empty spaces, the moonlit forests scattered their clear shadows.",
 "〚0042:5〛"=>"Constellations’ woof pressed close on Heaven’s towers, lying in the clouds, my clothes were cold.",
