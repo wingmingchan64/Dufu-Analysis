aa187317ba4622f7ecce6800e653f11c4ff429e4
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-02-23 08:21:59 -0600

    Updated

--- analysis_programs/檢查版本默認頁碼.php
+++ analysis_programs/檢查版本默認頁碼.php
@@ -1,12 +1,17 @@
 <?php
 /*
 php h:\github\Dufu-Analysis\analysis_programs\檢查版本默認頁碼.php
+
+檢查以下事項：
+1. 目錄内默認頁碼是否正確
+2. 格式是否正確
 */
 require_once( '函式.php' );
 require_once( 頁碼 );
 require_once( 書目簡稱 );
 
 $簡稱 = '訳';
+$簡稱 = '仇';
 $路徑 = 杜甫分析文件夾 . $書目簡稱[ '=' . $簡稱 ] . "\\${簡稱}目錄.txt";
 $file = file_get_contents( $路徑 );
 $lines = explode( NL, $file );
