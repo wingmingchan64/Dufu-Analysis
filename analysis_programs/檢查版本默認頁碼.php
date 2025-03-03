ecf21b1c3f53b4acf3f73ae80cdc132f4c90f77c
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-03-03 16:08:53 -0600

    Updated

--- analysis_programs/檢查版本默認頁碼.php
+++ analysis_programs/檢查版本默認頁碼.php
@@ -14,6 +14,13 @@ $簡稱 = '訳';
 $簡稱 = '仇';
 $簡稱 = '謝';
 $簡稱 = '朱';
+$簡稱 = '趙';
+$簡稱 = '趙';
+$簡稱 = '黃';
+$簡稱 = '錢';
+$簡稱 = '楊';
+$簡稱 = '郭';
+
 
 $路徑 = 杜甫分析文件夾 . $書目簡稱[ '=' . $簡稱 ] . "\\${簡稱}目錄.txt";
 $file = file_get_contents( $路徑 );
@@ -23,21 +30,35 @@ $counter = 0;
 foreach( $lines as $line )
 {
 	$counter++;
-	if( $line == '' || strpos( $line, '//' ) === false )
+	if( mb_strpos( $line, '==' ) !== false )
 	{
-		continue;
-	}
-	$parts = explode( ' ', $line );
-	$默認頁碼 = trim( $parts[ 2 ] );
-	
-	if( in_array( $默認頁碼, $頁碼 ) || $默認頁碼 == '6497' )
-	{
-		continue;
-	}
-	else
-	{
-		echo $counter, ' ', $默認頁碼, NL;
 		exit;
+	}
+	elseif( $line == '' || strpos( $line, '//' ) === false )
+	{
+		continue;
+	}
+
+	$parts = explode( ' ', $line );
+	
+	try
+	{
+		$默認頁碼 = trim( $parts[ 2 ] );
+		
+		if( in_array( $默認頁碼, $頁碼 ) || $默認頁碼 == '6497' )
+		{
+			continue;
+		}
+		else
+		{
+			echo $counter, ' ', $默認頁碼, NL;
+			exit;
+		}
+	}
+	catch( ErrorException $e )
+	{
+		echo $counter, NL;
+		echo $e;
 	}
 }
 echo "Done", NL;
