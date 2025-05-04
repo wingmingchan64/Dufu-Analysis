05344fb95e001aa4cd5ba220ead8119fead7c456
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-05-04 13:37:45 -0500

    Updated

---
+++ 《地名詞典》/舍衞國.php
@@ -0,0 +1,9 @@
+<?php
+$舍衞國=array(
+定義=>"當代名稱：Shravasti
+Shravasti is a town in Shravasti district in Indian state of Uttar Pradesh. It was the capital of the ancient Indian kingdom of Kosala which was ruled by the Lava and the place where the Buddha lived most after his enlightenment.
+地理位置：印度東北偏北，接近尼泊爾。
+坐標：27.50691647288002, 82.03392396843458",
+參考=>array("")
+);
+?>
