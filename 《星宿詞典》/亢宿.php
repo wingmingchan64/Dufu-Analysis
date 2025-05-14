7b1b2c3e826b6feae5b5fb868d89ecf82742a505
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-05-14 17:47:49 -0500

    Updated

---
+++ 《星宿詞典》/亢宿.php
@@ -0,0 +1,8 @@
+<?php
+$亢宿=array(
+定義=>"室女座/處女座 Virgo 的室女座 Virgo κ Kappa Virginis
+
+https://en.wikipedia.org/wiki/Kappa_Virginis#:~:text=Kappa%20Virginis%20(%CE%BA%20Virginis%2C%20abbreviated,is%20about%20255%20light%2Dyears.",
+參考=>array("")
+);
+?>
---
+++ 《星宿詞典》/角宿.php
@@ -0,0 +1,8 @@
+<?php
+$角宿=array(
+定義=>"室女座/處女座 Virgo 的室女座α Spica
+
+https://en.wikipedia.org/wiki/Spica#:~:text=Spica%20is%20the%20brightest%20object,of%20the%20Beta%20Cephei%20type.",
+參考=>array("")
+);
+?>
