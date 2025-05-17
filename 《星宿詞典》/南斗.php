21a6e23542710bf3f0e03aef14eb8924c9caf08d
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-05-17 12:31:39 -0500

    Updated

---
+++ 《星宿詞典》/南斗.php
@@ -0,0 +1,9 @@
+<?php
+$南斗=array(
+定義=>"射手座/人馬座 Sagittarius （像茶壺）的一部分
+Nanto φ 天同, Kaus Borealis λ 天相 （壺蓋頂）, Polis μ 七殺, Nunki σ 天機, Hecatebolus τ 天梁, Ascella ζ 天府。 茶壺的手把、蓋子的一部分，還有蓋子上面的 μ
+天機：18h55m15.93m -26°17′48.2″
+",
+參考=>array("")
+);
+?>
