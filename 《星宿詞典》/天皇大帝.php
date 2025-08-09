951dfacd7269769d5352c96bfeb92a91dd5d78ca
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-08-09 18:59:14 -0500

    Updated

---
+++ 《星宿詞典》/天皇大帝.php
@@ -0,0 +1,13 @@
+<?php
+$天皇大帝=array(
+定義=>"HR 8546，小熊座 Ursa Minor
+古代中國星官之一，位於紫微垣中，被勾陳所包圍著，是佔星術中天帝的象徵。
+RA:  22h 13m 10.51s
+DEC: +86°06′28.81″
+https://zh.wikipedia.org/wiki/%E5%A4%A9%E7%9A%87%E5%A4%A7%E5%B8%9D_(%E6%81%92%E6%98%9F)
+https://zh.wikipedia.org/wiki/%E5%B0%8F%E7%86%8A%E5%BA%A7
+https://simbad.u-strasbg.fr/simbad/sim-id?Ident=HR+8546
+",
+參考=>array("")
+);
+?>
