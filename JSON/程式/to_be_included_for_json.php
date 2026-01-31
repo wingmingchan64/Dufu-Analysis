735eab0a4ce15a30cd06d36a9d836c313db897ca
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-01-31 14:48:09 -0600

    Updated

--- JSON/程式/to_be_included_for_json.php
+++ JSON/程式/to_be_included_for_json.php
@@ -18,18 +18,10 @@ require_once(
 	"常數.php" );
 require_once( 程式文件夾 . "函式.php" );
 require_once( 
-	"H:" . DIRECTORY_SEPARATOR .
-	"github" . DIRECTORY_SEPARATOR .
-	"Dufu-Analysis" . DIRECTORY_SEPARATOR .
-	"JSON" . DIRECTORY_SEPARATOR .
-	"程式" . DIRECTORY_SEPARATOR .
-	"loader.php" );
+	杜甫分析文件夾 .
+	"JSON" . DS . "程式" . DS . "loader.php" );
 $JSON_BASE = 
-	"H:" . DIRECTORY_SEPARATOR .
-	"github" . DIRECTORY_SEPARATOR .
-	"Dufu-Analysis" . DIRECTORY_SEPARATOR .
-	"JSON" . DIRECTORY_SEPARATOR .	
-	"數據結構";
+	杜甫分析文件夾 . "JSON" . DS . "數據結構";
 $DATA = new JsonDataLoader( $JSON_BASE );
 $默認詩文檔碼 = $DATA->get( 默認詩文檔碼 );
 $默認詩文檔碼_詩題 = $DATA->get( 默認詩文檔碼_詩題 );
@@ -41,4 +33,14 @@ $行碼_詩文 = $DATA->get( 行碼_詩文 );
 $行碼_副題 = $DATA->get( "行碼_副題" );
 $句碼_詩句 = $DATA->get( "句碼_詩句" );
 $杜甫詩陣列 = $DATA->get( "杜甫詩陣列" );
+
+//蕭滌非
+$蕭JSON_BASE = 
+	杜甫分析文件夾 . "JSON" . DS . "蕭滌非《杜甫全集校注》";
+$蕭DATA = new JsonDataLoader( $蕭JSON_BASE );
+$默詩碼_蕭詩碼 = $蕭DATA->get( 默詩碼_蕭詩碼 );
+$蕭詩碼_默詩碼 = $蕭DATA->get( 蕭詩碼_默詩碼 );
+$詩題_蕭詩碼 = $蕭DATA->get( 詩題_蕭詩碼 );
+$蕭詩碼_詩題 = $蕭DATA->get( 蕭詩碼_詩題 );
+
 ?>
