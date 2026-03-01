90b10bd6441afbc4c4a80eb1ffa3374112742e56
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-03-01 09:04:38 -0600

    Updated

--- tools/php/bin/views/生成版本/生成《全唐詩》.php
+++ tools/php/bin/views/生成版本/生成《全唐詩》.php
@@ -12,6 +12,7 @@ $後設資料文件夾 = dirname( __DIR__, 5 ) . DS . PACKAGES_DIR .
 	'《全唐詩》' . DS . METADATA_DIR . 'by_doc_id' . DS;
 	
 $多個文檔 = false;
+$生成md = true;
 
 // 生成一個文檔，文檔碼爲：版本文檔碼
 if( !$多個文檔 )
@@ -28,9 +29,8 @@ else
 		throw new RuntimeException( '後設資料文件夾不存在: ' . 後設資料文件夾 );
 	}
 	
-	// scan the source folder
+	// scan the source folder and get the filenames
 	$files = scandir( $資料來源文件夾 );
-	//print_r( $files );
 	sort( $files, SORT_STRING );
 
 	foreach( $files as $file )
@@ -47,27 +47,27 @@ else
 	}
 }
 
-print_r( $版文檔碼s );
-
 foreach( $版文檔碼s as $版文檔碼 )
 {
-	echo $版文檔碼, NL;
 	// 處理 DuFu 中 .txt 文檔的〘〙標記，生成後設標記 JSON 文檔
 	處理後設標記( '全', $版文檔碼, '中華書局版', true, true );
 	處理後設資料( '全', $版文檔碼 );
 
-	$版本文檔 = 提取版本文檔( '全', $版文檔碼, true, true );
-	$詩陣列詩文 = 提取詩陣列詩文( $版本文檔, true, true );
-	$詩題 = "# " . 加sub標簽( $詩陣列詩文[ 0 ] );
-	unset( $詩陣列詩文[ 0 ] );
+	if( $生成md )
+	{
+		$版本文檔 = 提取版本文檔( '全', $版文檔碼, true, true );
+		$詩陣列詩文 = 提取詩陣列詩文( $版本文檔, true, true );
+		$詩題 = "# " . 加sub標簽( $詩陣列詩文[ 0 ] );
+		unset( $詩陣列詩文[ 0 ] );
 
-	$詩題 = $詩題 . NL . NL;
-	$詩文 = 加sub標簽( implode( NL, $詩陣列詩文 ) );
-	$contents = $詩題 . $詩文;
+		$詩題 = $詩題 . NL . NL;
+		$詩文 = 加sub標簽( implode( NL, $詩陣列詩文 ) );
+		$contents = $詩題 . $詩文;
 
-	file_put_contents(
-		dirname( __DIR__, 5 ) . DS . PACKAGES_DIR . 
-		'《全唐詩》' . DS . 'samples' . DS . "${版文檔碼}.md",
-		$contents . PHP_EOL );
+		file_put_contents(
+			dirname( __DIR__, 5 ) . DS . PACKAGES_DIR . 
+			'《全唐詩》' . DS . 'samples' . DS . "${版文檔碼}.md",
+			$contents . PHP_EOL );
+	}
 }
 ?>
