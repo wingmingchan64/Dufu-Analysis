634c63087dea4441ac1c578966f451cbb93bfc68
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-03-01 12:26:06 -0600

    Updated

--- analysis_programs/搜索程式/全集.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-php h:\github\Dufu-Analysis\analysis_programs\搜索程式\全集.php
-*/
-require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
-require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
-
-print_r( file_get_contents( 杜甫資料庫 . '杜甫全集無夾注.txt' ) );
-?>
--- analysis_programs/搜索程式/目錄.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-php h:\github\Dufu-Analysis\analysis_programs\搜索程式\目錄.php
-*/
-require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\常數.php" );
-require_once( "h:\\github\\Dufu-Analysis\\analysis_programs\\函式.php" );
-require_once( 頁碼_詩題 );
-print_r( $頁碼_詩題 );
-?>
