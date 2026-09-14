2dce2a6c443d91b2d4dad842ccf154a0b9123491
Wing Ming Chan <wingmingchan64@hotmail.com>
2026-09-14 13:10:35 -0500

    Updated

---
+++ tools/php/bin/views/默認版本定位/默文檔碼→行碼、詩文.php
@@ -0,0 +1,30 @@
+<?php
+/*
+php H:\github\Dufu-Analysis\tools\php\bin\views\默認版本定位\默文檔碼→行碼、詩文.php 3
+=> 
+*/
+require_once( 
+	dirname( __DIR__, 3 ) . DIRECTORY_SEPARATOR .
+	'lib' . DIRECTORY_SEPARATOR . '函式.php' );
+
+check_argv( $argv, 2, 提供默文檔碼 );
+$默文檔碼 = fix_doc_id( trim( $argv[ 1 ] ) );
+$默認詩文檔碼 = 提取數據結構( 默認詩文檔碼 );
+
+if( !in_array( $默文檔碼, $默認詩文檔碼 ) )
+{
+	echo 無頁碼, NL;
+}
+$詩文文檔路徑 = dirname( __DIR__, 6 ) . DIRECTORY_SEPARATOR .
+	默認版本詩文件夾 . $默文檔碼 . '.txt';
+$contents = file_get_contents( $詩文文檔路徑 );
+$lines = explode( NL, $contents );
+
+echo NL;
+
+for( $i = 0; $i < count( $lines ); $i++ )
+{
+	$line_num = $i + 1;
+	echo "〚${line_num}〛", $lines[ $i ], NL;
+}
+?>
